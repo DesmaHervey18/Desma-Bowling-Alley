8f9606fb508df2fd7193286262c4617e2a1358d6
Desma <dhervey@basecampcodingacademy.org>
2018-08-02 16:24:19 -0500

    class project

--- shell.py
+++ shell.py
@@ -1,39 +1,41 @@
 def welcome():
     print('~*~*~*~*Welcome to Desma\'s Amazing Bowling Alley~*~*~*~*~*~* \n')
-    response = input('Are you bowling alone today?\n')
-    if response == 'yes':
-        size = float(input('what is your size?\n'))
-    elif response == 'no':
-        print('Thank You for coming, Good day')
+    while True:
+        response = input('Do you want to bowl today?\n').upper().strip()
+        if response == 'YES':
+            break
+        elif response == 'NO':
+            print('Thank You for coming, Good day')
+        else:
+            print('*****Invalid Answer, Please Try Again*****')
 
 
 def Bowling_Alley():
-    choice = (input('Are you ready to bowl?\n'))
-    choice == 'yes'
-    print('ok, Let\'s get started...')
-    frame = 0
-    total = 0
-    for i in range(10):
-        print('_______________________________________________')
-        ball_1, ball_2 = 0, 0
-        ball_1 = int(input('\nWhat did you roll on the first roll? '))
-        frame = frame + 1
-        if ball_1 == 10:
-            print('~~**~STRIKE~**~~')
-        elif ball_1 < 10:
-            print('**SPARE**')
+    choice = input('Are you ready to bowl?\n')
+    if choice == 'YES':
+        print('ok, Let\'s get started...')
+        frame = 0
+        total = 0
+        for i in range(10):
+            print('_______________________________________________')
+            ball_1, ball_2 = 0, 0
+            frame = frame + 1
+            ball_1 = int(input('\nWhat did you roll on the first roll? '))
+            if ball_1 == 10:
+                print('~~**~STRIKE~**~~')
+                continue
+
             ball_2 = int(input('\nHow many pins fell on your roll? '))
-        print('\nFRAME:{}'.format(frame))
-        total += (ball_1 + ball_2)
-    print('Player Score: {}'.format(total))
+            if ball_1 + ball_2 == 10:
+                print('**SPARE**')
 
-
-# def bowler():
-#     total = 0
-#     for frame in frames:
-#         total += sum(frames)
-#         print(total)
-#     print(frame)
+            print('\nFRAME:{}'.format(frame))
+            total += (ball_1 + ball_2)
+        print('Player Score: {}'.format(total))
+    elif choice == 'NO':
+        exit()
+    else:
+        print('*****Invalid Answer, Please Try Again*****')
 
 
 def main():
